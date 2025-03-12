87d074008c2e009573c7ce491ac73a0012966782
Aniket8699 <157409504+Aniket8699@users.noreply.github.com>
2025-03-12 15:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('<h3>Welcome to Wordpress </h3>s')
 })
 
 app.listen(8080,function(){
